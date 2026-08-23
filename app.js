3449adbdd5f76cf197364bbb47c8619516393dc4
Kanak <kyadavcs01@gmail.com>
2026-08-23 19:13:16 +0530

    Update app message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello! Automated DevOps Deployment is Working!");
+    res.send("Hey this is KANAK YADAV!<br>The Automated DevOps Deployment is Working!");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
